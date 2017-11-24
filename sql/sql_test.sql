fe2eec4ea4a069f49992312aaabeeedbdb21c2ed
PC-201704171401\Administrator <867999030@qq.com>
2017-11-24 16:12:46 +0800

    no message

--- sql/sql_test.sql
+++ sql/sql_test.sql
@@ -1,4 +1,4 @@
-//分组条件筛选（统计每一个title的数量并用group_concat把所有id和title组合选出来）
+
 CREATE TABLE `im_category` (
   `cate_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(32) NOT NULL DEFAULT '' COMMENT '标题',
@@ -20,7 +20,7 @@ INSERT INTO `im_category` VALUES ('7', '酷派', '2', '1510072826');
 INSERT INTO `im_category` VALUES ('8', '服装', '0', '1510107152');
 INSERT INTO `im_category` VALUES ('9', '宠物', '0', '1510199888');
 INSERT INTO `im_category` VALUES ('10', '狗狗', '9', '1510199904');
-
+//分组条件筛选（统计每一个title的数量并用group_concat把所有id和title组合选出来）
 SELECT COUNT(title) AS title_num, GROUP_CONCAT(CONCAT_WS(':',cate_id,title) SEPARATOR ', ') AS id_title, pid from im_category where create_time >= 1510052491 GROUP BY pid;
 
 
